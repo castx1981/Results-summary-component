b049dacd507ebb8d989f98cda13b93f7b9964d68
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-15 23:52:11 +0800

    replace tailwind CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['"Hanken Grotesk"', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        // primary
+        'light-red': 'hsl(0, 100%, 67%)',
+        'orange-yellow': 'hsl(39, 100%, 56%)',
+        'green-teal': 'hsl(166, 100%, 37%)',
+        'cobalt-blue': 'hsl(234, 85%, 45%)',
+        // neutral
+        'pale-blue': 'hsl(221, 100%, 96%)',
+        'light-lavender': 'hsl(241, 100%, 89%)',
+        'dark-gray-blue': 'hsl(224, 30%, 27%)',
+        // gradients
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsla(241, 72%, 46%, 0)',
+      },
+      backgroundImage: {
+        'mobile-pattern': "url('images/pattern-background-mobile.svg')",
+        'desktop-pattern': "url('images/pattern-background-desktop.svg')",
+      }
+    }
   },
   plugins: [],
 }
